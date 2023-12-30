8b735b46c98c16e3806aaecfda9307bbe3d28682
Lohan Almeida <150105725+LohanAlmeida@users.noreply.github.com>
2023-12-30 19:41:12 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,55 @@
+const button = document.querySelector('.button-add-task')
+const input = document.querySelector('.input-task')
+const listaCompleta = document.querySelector('.list-tasks')
+
+let minhaListaDeItens = []
+
+function adicionarNovaTarefa() {
+    minhaListaDeItens.push({
+        tarefa: input.value,
+        concluida: false
+    })
+
+    input.value = ''
+
+    mostrarTarefas()
+
+}
+
+function mostrarTarefas() {
+
+    let novaLi = ''
+
+    // ['comprar café', 'estudar programação']
+
+    minhaListaDeItens.forEach((item, posicao) => {
+        novaLi = novaLi + `
+
+        <li class="task">
+        <img src="img/checked.png" alt="check-na-tarefa" onclick="concluirTarefa(${posicao})/>
+        <p>${item.tarefa}</p>
+        <img src="img/trash.png" alt="tarefa-para-o-lixo" onclick="deletarItem(${posicao})" />
+      </li>
+      `
+
+    })
+
+
+    listaCompleta.innerHTML = novaLi
+
+}
+
+function concluirTarefa() {
+
+
+
+}
+
+function deletarItem(posicao) {
+    minhaListaDeItens.splice(posicao, 1)
+
+    mostrarTarefas()
+}
+
+
+button.addEventListener('click', adicionarNovaTarefa)
